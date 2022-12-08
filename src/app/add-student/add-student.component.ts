9f307dc005bd6f62c4aec1ed37a7e98c0a512cba
sufailbadushat <sufailbadushat@gamil.com>
2022-12-08 16:15:36 +0530

    add studnent to database

---
+++ src/app/add-student/add-student.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
+
+@Component({
+  selector: 'app-add-student',
+  templateUrl: './add-student.component.html',
+  styleUrls: ['./add-student.component.css']
+})
+export class AddStudentComponent {
+  constructor(private api: ApiService) { }
+
+  name = ""
+  admNo = ""
+  rollNo = ""
+  collegeName = ""
+
+
+  readValue = () => {
+    let data: any = {
+      "name": this.name,
+      "admNo":this.admNo,
+      "rollNo":this.rollNo,
+      "collegeName":this.collegeName
+    }
+    console.log(data);
+
+    this.api.addStudents(data).subscribe(
+      (response:any)=>{
+        if (response.status == "success") {
+          alert("Product added successfully!");
+          this.name = ""
+          this.admNo = ""
+          this.rollNo = ""
+          this.collegeName = ""
+        
+        }
+        else{
+          alert("something went wrong!");
+        }
+      }
+    )
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddStudentComponent } from './add-student/add-student.component';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
+
+let myRoot:Routes=[
+  {
+    path:"",
+    component:AddStudentComponent
+  }
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddStudentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRoot),
+    FormsModule,
+    HttpClientModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
